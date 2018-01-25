4c5a3faca87b166926bd2bcc114a28ead6f200fa
Abebe Asnake <abexasnex@gmail.com>
2018-01-25 18:30:13 -0500

    using @Requestmapping

--- src/main/java/com/abebe/demospringboot/MainController.java
+++ src/main/java/com/abebe/demospringboot/MainController.java
@@ -2,14 +2,14 @@ package com.abebe.demospringboot;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
 import java.util.Scanner;
 
 @RestController
 public class MainController {
 
-    @RequestMapping(value="/home/data/la")
+    @RequestMapping(value="/")
     public String ShowIndex()
     {
         String userName;
@@ -19,6 +19,23 @@ public class MainController {
         System.out.println(greet);
         System.out.println("hello sir");
         return  greet;
+    }
+    @RequestMapping("/getname")
+    public String showWithName(@RequestParam("name") String name,@RequestParam("age") int age,@RequestParam("color") String color)
+    {
+
+        return name+" "+age+"" + ""+color;
+    }
+
+
+    @RequestMapping("/getothername")
+    public String showWithName(HttpServletRequest param)
+    {
+
+        String withName = param.getParameter("yourname");
+        if(withName==null)
+            return "Just Hello";
+        else return "Hello "+withName;
     }
 
 }
